c77f3e95b732ad8dfb0cc4163f16c4bec1e1f1c0
Rajesh Yemineni <73592971+rajeshy45@users.noreply.github.com>
2023-04-17 19:44:28 +0530

    created city wise prices table

---
+++ db/deltas/01_create_city_wise_prices.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `city_wise_prices` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(255) NOT NULL,
+  `price` int NOT NULL,
+  `date` date NOT NULL DEFAULT current_date(),
+  `last_updated` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+);
