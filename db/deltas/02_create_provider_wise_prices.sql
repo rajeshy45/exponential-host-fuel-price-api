2c384cdc11c10e3a60fdbb29733a86a896dd1b61
Vikasguggilam14 <saivikas_guggilam@srmap.edu.in>
2023-04-19 19:54:28 +0530

    updated sql files

--- db/deltas/02_create_provider_wise_prices.sql
+++ db/deltas/02_create_provider_wise_prices.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `provider_wise_prices` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `provider_name` varchar(255) NOT NULL,
+  `provider` varchar(255) NOT NULL,
   `price` int NOT NULL,
   `date` date NOT NULL DEFAULT current_date(),
   `last_updated` datetime NOT NULL DEFAULT current_timestamp(),
