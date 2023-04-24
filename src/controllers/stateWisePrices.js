289428c341ae14fc8feab3c6880d32f70bcef765
Rajesh Yemineni <73592971+rajeshy45@users.noreply.github.com>
2023-04-24 11:08:25 +0530

    implemented get route for state wise prices

--- src/controllers/stateWisePrices.js
+++ src/controllers/stateWisePrices.js
@@ -1,16 +1,25 @@
 const StateWisePricesModel = require("../models/stateWisePrices");
 
-
-
 const StateWisePrices = {
   getRecord: async (req, res) => {
-    res.status(201).send();
+    let query = req.query;
+    console.log(query);
+
+    const records = await StateWisePricesModel.findAll({
+      order: [
+        ["state", "ASC"],
+        ["date", "DESC"],
+        ["last_updated", "DESC"],
+      ],
+    });
+
+    res.json(records);
   },
 
   insertRecord: async (req, res) => {
     let data = req.body;
     console.log(data);
-    
+
     const record = await StateWisePricesModel.create(data);
 
     res.json(record);
